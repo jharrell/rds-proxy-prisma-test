a8d25f20973997da6b399d21f55ffa7f3e6e72aa
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2023-11-27 10:25:24 -0600

    async await instead of then/catch

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -3,24 +3,20 @@ import { ApiHandler } from "sst/node/api";
 
 export const handler = ApiHandler(async (_evt) => {
   console.log('starting handler! attempting to fetch users.');
-
-  // const users = await 
-  prisma.user.findMany({ 
-    include: {
-      posts: true
-    }
-  }).then((users) => {
-    const body = `Hello world. The time is ${new Date().toISOString()}. There are ${users.length} users. The last user's username is ${users.at(-1)?.username}`
+  try {
+    const users = await prisma.user.findMany({ include: { posts: true }});
+    console.log(`Users found! Returning info about ${users.length} users.`);
+    const body = `Hello world. The time is ${new Date().toISOString()}. There are ${users.length} users. The latest user's username is ${users.at(-1)?.username}`
     return {
       statusCode: 200,
       body,
     };
-  }).catch((err) => {
+  } catch (error) {
+    console.error('uh oh.');
+    console.error(error);
     return {
       statusCode: 500,
-      err,
+      error,
     }
-  })
-  
-
+  }
 });
